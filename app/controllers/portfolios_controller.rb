e71f77d8edff0fe1c1ae8fda682873734ed62a06
Jordan Hudgens <jordan@devcamp.com>
2017-07-11 16:49:26 +0000

    Implemented image defaults for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,31 +3,6 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
-  def angular 
-    @angular_portfolio_items = Portfolio.angular
-  end
-  
-  def new
-    @portfolio_item = Portfolio.new
-  end
-
-  def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
-
-    respond_to do |format|
-      if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
-      else
-        format.html { render :new }
-      end
-    end
-  end
-
-class PortfoliosController < ApplicationController
-  def index
-    @portfolio_items = Portfolio.all
-  end
-
   def angular
     @angular_portfolio_items = Portfolio.angular
   end
@@ -79,6 +54,6 @@ class PortfoliosController < ApplicationController
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
-  end 
-end 
-end 
+  end
+
+end
