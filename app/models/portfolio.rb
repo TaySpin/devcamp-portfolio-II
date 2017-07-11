8dc2cb6d7c047101078234acac03421733c82c42
Jordan Hudgens <jordan@devcamp.com>
2017-07-11 15:49:57 +0000

    Implemented custom scopes for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,20 @@
 class Portfolio < ApplicationRecord
-  
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  has_many :technologies
+  accepts_nested_attributes_for :technologies,
+                                allow_destroy: true,
+                                reject_if: lambda { |attrs| attrs['name'].blank? }
+
+  validates_presence_of :title, :body
+
+
+
+  def self.angular
+    where(subtitle: 'Angular')
+  end
+
+  def self.by_position
+    order("position ASC")
+  end
+
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
 end
