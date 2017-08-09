439e38d28ea3e0dfb2e9b6312683b83cffe4713a
Jordan Hudgens <jordan@devcamp.com>
2017-08-09 15:58:18 +0000

    Implemented authorization engine for porfolios and blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,9 +1,10 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
   layout "blog"
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+
   # GET /blogs
   # GET /blogs.json
-  
   def index
     @blogs = Blog.special_blogs
     @page_title = "My Portfolio Blog"
@@ -39,7 +40,7 @@ class BlogsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /blogs/1s
+  # PATCH/PUT /blogs/1
   # PATCH/PUT /blogs/1.json
   def update
     respond_to do |format|
